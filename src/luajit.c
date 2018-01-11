8f8f67bcbd6bb6275c818c38161725a9d7cbb7d4
Hanno Behrens <behrens.hanno@gmail.com>
2018-01-11 15:41:26 +0100

    interactive commandline with readline(), Hanno Behrens

--- src/luajit.c
+++ src/luajit.c
@@ -9,6 +9,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
 #define luajit_c
 
@@ -190,15 +192,16 @@ static int dolibrary(lua_State *L, const char *name)
   return report(L, docall(L, 1, 1));
 }
 
-static void write_prompt(lua_State *L, int firstline)
+static const char *get_prompt(lua_State *L, int firstline)
 {
-  const char *p;
+  static const char *p=NULL;
   lua_getfield(L, LUA_GLOBALSINDEX, firstline ? "_PROMPT" : "_PROMPT2");
   p = lua_tostring(L, -1);
-  if (p == NULL) p = firstline ? LUA_PROMPT : LUA_PROMPT2;
-  fputs(p, stdout);
-  fflush(stdout);
+  if(p == NULL) p = firstline ? LUA_PROMPT : LUA_PROMPT2;
+  //fputs(p, stdout);
+  //fflush(stdout);
   lua_pop(L, 1);  /* remove global */
+  return p;
 }
 
 static int incomplete(lua_State *L, int status)
@@ -217,9 +220,12 @@ static int incomplete(lua_State *L, int status)
 
 static int pushline(lua_State *L, int firstline)
 {
-  char buf[LUA_MAXINPUT];
-  write_prompt(L, firstline);
-  if (fgets(buf, LUA_MAXINPUT, stdin)) {
+//  char buf[LUA_MAXINPUT];
+  char *buf=NULL;
+//  write_prompt(L, firstline);
+//  if (fgets(buf, LUA_MAXINPUT, stdin)) {
+  if ((buf=readline(get_prompt(L, firstline)))!=NULL) {
+	add_history(buf);
     size_t len = strlen(buf);
     if (len > 0 && buf[len-1] == '\n')
       buf[len-1] = '\0';
@@ -227,6 +233,7 @@ static int pushline(lua_State *L, int firstline)
       lua_pushfstring(L, "return %s", buf+1);
     else
       lua_pushstring(L, buf);
+	free(buf);
     return 1;
   }
   return 0;
